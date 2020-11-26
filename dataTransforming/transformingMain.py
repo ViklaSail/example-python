612fdb8f1c77dfc101a36511729c23c7d9fae17a
PekkaR <ve.pe.re@hotmail.com>
2020-11-26 08:53:49 +0200

    g

--- dataTransforming/transformingMain.py
+++ dataTransforming/transformingMain.py
@@ -1,4 +1,19 @@
-def transformationForData(row):
-    # Should return transformed data, this one works line by line modifying data. 
-    transformedData="something"
-    return transformedData
+
+import numpy as np
+def transformationForData(cell):
+    # Should return transformed data, this one works line by line modifying data. Now t
+    #transformedData="something"
+    result = np.square(cell) if cell.name == 'A' else cell
+    result2 = result + 10 if cell.name == 'B' else result
+    return result2
+
+def transformationTestingWithApply(cell):
+    #https://thispointer.com/pandas-apply-a-function-to-single-or-selected-columns-or-cells-in-dataframe/
+    # so called ternary if operations used in example, makes also code more readable as well as short
+    result = np.square(cell) if cell.name == 'A' else cell
+    result2 = columnBHandling(cell) if cell.name == 'B' else result
+    return result2
+
+def columnBHandling(cell):
+    result = cell - 5
+    return result
--- testsArchive.py
+++ testsArchive.py
@@ -2,10 +2,22 @@ import unittest
 from mock import patch
 
 import awesome
+# import example_pkg
+# 12.1.2020 something close to functional programming
+# map, reduce and filter operations
+# https://stackoverflow.com/questions/31127824/what-are-pythons-equivalent-of-javascripts-reduce-map-and-filter
+# for dataframe there is this functionality build in 
+# DataFrame.apply, DataFrame.applymap
+# datasets for cleaning exercises: https://makingnoiseandhearingthings.com/2018/04/19/datasets-for-data-cleaning-practice/
+# 11.1.2020 utest and related. 
+# from app.mocking import test_method
+# for mocking to work you need to import the function to be tested to the same namespace with unit tests!!!
+# this is wy below from-sentences  are made.
+# that is my understanding, untested.
 from example_pkg import laughingChain
 from example_pkg import laughingChainPara
 from example_pkg import funcSameNameSpace
-# if patching a function in the same package, mocking/patching does not work!!! You will have to test the whole function chain
+# if testing a function in the same namespace, it does not work!!! trying to find solution...
 # https://stackoverflow.com/questions/46464947/python-function-not-using-the-mocked-object
 # possible solution maybe here https://docs.python.org/3/library/unittest.mock.html#where-to-patch
 from example_pkg import foo_func
@@ -16,21 +28,25 @@ from dataTransforming import transformationTestingWithApply
 from functools import reduce
 import pandas as pd
 import numpy as np
+# import pipeLine
 
 url = 'http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json'
 
-class TestMethods_live(unittest.TestCase):
-    def test_assertion_1(self):
+class TestMethods(unittest.TestCase):
+    def test_add(self):
         # call to function changing some "global" data structure 
         # assert function is checking that the change you expected happens properly. 
         self.assertEqual(awesome.smile(), ":)")
   
-    def test_assertion_2(self):
+    def test_anotherOne(self):
         barres = bar_func()
         self.assertRegexpMatches(barres,".*bar.*")
+    
+    #def test_pipeline(self):
+    #    result = pipeLine.getData(url)
+    #    self.assertRegexpMatches(result,".*bar.*")
 
-
-    def test_assertion_3(self):
+    def test_laugh(self):
         reslau = awesome.laughing()
         self.assertEqual(reslau, ":D")
 
@@ -46,24 +62,43 @@ class TestMethods_live(unittest.TestCase):
     def test_parameterfunc(self, test_para):
         test_para.return_value = "HUI"
         result  = laughingChainPara()
-        self.assertEqual(result,"testing")
+        self.assertEqual(result,"testausta")
 
     @patch('example_pkg.foo_func')
     def test_samemodule(self, test_para):
         test_para.return_value = "SAME NAMESPACE"
         result  = funcSameNameSpace()
-        self.assertEqual(result,"testing")
+        self.assertEqual(result,"testausta")
+
+    def test_playdata(self):
+        #https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas first version
+        # 
+        from functools import reduce
+        import pandas as pd
+        import numpy as np
+        df = pd.DataFrame({'c1': [10, 11, 12], 'c2': [100, 110, 120]})
+        for index, row in df.iterrows():
+            print(row['c1'], row['c2'])
+        df.applymap
 
     def test_framingApply(self):
         #https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas first version
+        # 
         df = pd.DataFrame({'c1': [10, 11, 12], 'c2': [100, 110, 120]})
-        for row in df.iterrows():
+        for index, row in df.iterrows():
             print(row['c1'], row['c2'])
         
         df.apply(transformationForData, axis=0, raw=False, result_type=None, args=())
         #DataFrame.apply(func, axis=0, raw=False, result_type=None, args=(), **kwds)
+        #transformingMain("testing")
+        bar_func()
 
-    def test_exploringFunctionChain(self):
+    def test_sanity1(self):
+        result = bar_func()
+        result = transformationForData("testing")
+        self.assertEqual(result,"testausta")
+
+    def test_sanity2(self):
         # preparing data for exploring
         df = pd.DataFrame([[4, 9]] * 3, columns=['A', 'B'])
         # exploring
